097867b1036f7aebfc38ccf73b64824155f2bd16
IndrePet <indrepetrausku@gmail.com>
2022-03-03 17:08:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+function makeSound(key) {
+  let audio;
+  switch (key) {
+    case 'w':
+      audio = new Audio('./sounds/crash.mp3');
+      audio.play();
+      break;
+    case 'a':
+      audio = new Audio('./sounds/kick-bass.mp3');
+      audio.play();
+      break;
+    case 's':
+      audio = new Audio('./sounds/snare.mp3');
+      audio.play();
+      break;
+    case 'd':
+      audio = new Audio('./sounds/tom-1.mp3');
+      audio.play();
+      break;
+    case 'j':
+      audio = new Audio('./sounds/tom-2.mp3');
+      audio.play();
+      break;
+    case 'k':
+      audio = new Audio('./sounds/tom-3.mp3');
+      audio.play();
+      break;
+    case 'l':
+      audio = new Audio('./sounds/tom-4.mp3');
+      audio.play();
+      break;
+    default:
+      console.log(key);
+  }
+}
+
+function buttonAnimation(currentKey) {
+  let activeButton = document.querySelector('.' + currentKey);
+  activeButton.classList.toggle('pressed');
+
+  setTimeout(() => {
+    activeButton.classList.remove('pressed');
+  }, 100);
+}
+
+const btn = document.querySelectorAll('.drum');
+
+btn.forEach((e) => {
+  e.addEventListener('click', function () {
+    let buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+});
+
+document.addEventListener('keydown', function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
